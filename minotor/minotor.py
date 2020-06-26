7f6c53acedff6401a47e9edcf32fd85158b3df67
Gauthier Chaty <gauthier.chaty@gmail.com>
2020-06-26 10:54:43 +0200

    update to minotor package

--- minotor/minotor.py
+++ minotor/minotor.py
@@ -1,36 +1,37 @@
-"""Main module."""
+"""Minotor module."""
+
 from datetime import datetime
 
 import requests
 
-from minotor import cpu
+from minotor import cpu, ram
 
 class Minotor:
 
     def __init__(self, port=8085):
 
-
         self.port = port
-        self.data, self.timestamp = self.download_data(port)
-        self.cpu = cpu.CPU(self.data, self.timestamp)
 
-    
-    def download_data(self, port):
-        
-        url = 'http://192.168.0.27:{}/data.json'.format(port)
+        self.data, self.timestamp = self.download_data()
+        self.cpu = cpu.CPU(self.data)
+        self.ram = ram.RAM(self.data)
+
+    def download_data(self):
+
+        url = 'http://192.168.0.27:{}/data.json'.format(self.port)
         try:
-            timestamp = int(datetime.timestamp(datetime.now()))
-            
+            date_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+
             response = requests.get(url)
             response.raise_for_status()
-            
-            return (response.json(), timestamp)
-            
+
+            return (response.json(), date_time)
+
         except requests.exceptions.HTTPError as err:
             print(err)
-            return (None, timestamp)
-    
+            return (None, date_time)
+
     def update(self):
 
-        self.data, self.timestamp = self.download_data(self.port)
+        self.data, self.timestamp = self.download_data()
         self.cpu = self.cpu.update(self.data, self.timestamp)
