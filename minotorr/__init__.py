1bb84b4455813886c344eb6a8833e9da373bc75e
Gauthier Chaty <gauthier.chaty@gmail.com>
2020-07-07 14:35:44 +0200

    fix release version

--- minotorr/__init__.py
+++ minotorr/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Minotorr."""
 
 __author__ = """Gauthier Chaty"""
-__email__ = 'gauthier.chaty@outlook.com'
-__version__ = '0.5.0'
+__email__ = 'gauthier.chaty+github@outlook.com'
+__version__ = '0.6.1'
