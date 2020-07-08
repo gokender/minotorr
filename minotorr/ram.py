90cb82e065ca8ea45355572e7998b63d257a1ce8
Gauthier Chaty <gauthier.chaty@gmail.com>
2020-07-08 14:20:28 +0200

    Update RAM class structure & unit tests

--- minotorr/ram.py
+++ minotorr/ram.py
@@ -1,68 +1,150 @@
-"""Hardware component module"""
+"""Ram component module"""
+class _Load:
+    """Private class for creating a Load object.
+
+    Provides RAM Load informations from Libre Hardware Monitor webserver.
+
+    :param memory (float):
+    :param virtual_memory (float):
+    :param unit (str):
+    """
+    def __init__(self, memory=0.0, virtual_memory=0.0, unit='%'):
+
+        self.memory = memory
+        self.virtual_memory = virtual_memory
+        self.unit = unit
+
+    def parse(self, data):
+        """Get RAM/Load subtree from data dictionnary"""
+        for core in data:
+            value = float(core['Value'].rstrip(' %').replace(',', '.'))
+            if core['Text'] == 'Memory':
+                self.memory = value
+            elif core['Text'] == 'Virtual Memory':
+                self.virtual_memory = value
+
+    def to_dict(self):
+        """Transform the _Load object to dict
+        :rtype: dict
+        """
+        data = {}
+
+        data['memory'] = self.memory
+        data['virtual_memory'] = self.virtual_memory
+        
+        data['unit'] = self.unit
+   
+        return data
+
+class _Data:
+    """Private class for creating a Data object.
+
+    Provides RAM Data informations from Libre Hardware Monitor webserver.
+
+    :param memory_used (float):
+    :param virtual_memory (float):
+    :param virtual_memory_used (float):
+    :param virtual_memory_available (float):
+    :param unit (str):
+    """
+    def __init__(self, memory_used=0.0, 
+                       memory_available=0.0,
+                       virtual_memory_used=0.0,
+                       virtual_memory_available=0.0,
+                       unit='GB'):
+
+        self.memory_used = memory_used
+        self.memory_available = memory_available
+        self.virtual_memory_used = virtual_memory_used
+        self.virtual_memory_available = virtual_memory_available
+        self.unit = unit
+
+    def parse(self, data):
+        """Get RAM/Data subtree from data dictionnary"""
+        for core in data:
+            value = float(core['Value'].rstrip(' GB').replace(',', '.'))
+            if core['Text'] == 'Memory Used':
+                self.memory_used = value
+            elif core['Text'] == 'Memory Available':
+                self.memory_available = value
+            elif core['Text'] == 'Virtual Memory Used':
+                self.virtual_memory_used = value
+            elif core['Text'] == 'Virtual Memory Available':
+                self.virtual_memory_available = value
+
+    def to_dict(self):
+        """Transform the _Data object to dict
+        :rtype: dict
+        """
+        data = {}
+
+        data['memory_used'] = self.memory_used
+        data['memory_available'] = self.memory_available
+        data['virtual_memory_used'] = self.virtual_memory_used
+        data['virtual_memory_available'] = self.virtual_memory_available
+        
+        data['unit'] = self.unit
+   
+        return data
 
 class RAM:
-    """"RAM Object"""
+    """Class for creating a RAM object.
+
+    Provides RAM informations from Libre Hardware Monitor webserver.
+
+    Basic Usage::
+
+      >>> from minotorr import ram
+      >>> ram_ob = ram.RAM(data) # data is the response from HTTP requests
+      >>> ram_ob.load
+      <minotorr.ram._Load>
+      >>> ram_ob.data
+      <minotorr.ram._Data>
+    """
 
     def __init__(self, data):
 
         self.parsed_data = {}
-
-        self.loads = {}
-        self.data = {}
+        
+        self.data = _Data()
+        self.load = _Load()
 
         self.update(data)
 
     def parse(self):
+        """Get RAM subtree from data dictionnary"""
         for component in self.raw_data['Children'][0]['Children']:
             if component['ImageURL'] == 'images_icon/ram.png':
                 self.parsed_data = component
 
-    def parse_loads(self):
+    def parse_load(self):
+        """Get load informations from RAM"""
         for category in self.parsed_data['Children']:
             if category['Text'] == 'Load':
-                for core in category['Children']:
-                    values_temp = {
-                        'min': float(core['Min'].rstrip(' %').replace(',', '.')),
-                        'value': float(core['Value'].rstrip(' %').replace(',', '.')),
-                        'max': float(core['Max'].rstrip(' %').replace(',', '.')),
-                        'unit':'%'
-                    }
-                    if core['Text'] == 'Memory':
-                        self.loads['memory'] = values_temp
-                    elif core['Text'] == 'Virtual Memory':
-                        self.loads['virtual_memory'] = values_temp
+                self.load.parse(category['Children'])
 
     def parse_data(self):
+        """Get data informations from RAM"""
         for category in self.parsed_data['Children']:
             if category['Text'] == 'Data':
-                for core in category['Children']:
-                    values_temp = {
-                        'min': float(core['Min'].rstrip(' GB').replace(',', '.')),
-                        'value': float(core['Value'].rstrip(' GB').replace(',', '.')),
-                        'max': float(core['Max'].rstrip(' GB').replace(',', '.')),
-                        'unit':'GB'
-                    }
-
-                    if core['Text'] == 'Memory Used':
-                        self.data['memory_used'] = values_temp
-                    elif core['Text'] == 'Memory Available':
-                        self.data['memory_available'] = values_temp
-                    elif core['Text'] == 'Virtual Memory Used':
-                        self.data['virtual_memory_used'] = values_temp
-                    elif core['Text'] == 'Virtual Memory Available':
-                        self.data['virtual_memory_available'] = values_temp
+                self.data.parse(category['Children'])
 
     def to_dict(self):
+        """Transform the RAM object to dict
+        :rtype: dict
+        """
         ram = {}
-        ram['loads'] = self.loads
-        ram['data'] = self.data
+        ram['load'] = self.load.to_dict()
+        ram['data'] = self.data.to_dict()
 
         return ram
 
     def update(self, data):
-
+        """Update RAM Object with new data
+        :param data: Response in JSON from HTTP webserver
+        """
         self.raw_data = data
         self.parse()
-        self.parse_loads()
+        self.parse_load()
         self.parse_data()
         
--- tests/test_ram.py
+++ tests/test_ram.py
@@ -10,28 +10,38 @@ class TestRam(unittest.TestCase):
         """Set up test fixtures"""
         self.ram = ram.RAM(data_testing.CPU_DATA)
 
-    def test_001_ram_data(self):
+    def test_ram_data(self):
         """Test if it's the right hardware component"""
         self.assertEqual(self.ram.parsed_data['ImageURL'], 'images_icon/ram.png')
 
-    def test_002_loads_parsed(self):
-        """Test if not empty""" #TODO : change doc
-        self.assertNotEqual(self.ram.loads, {})
+    def test_load_memory(self):
+        """Test the load memory"""
+        self.assertEqual(self.ram.load.memory, 82.9)
+    
+    def test_load_virtual_memory(self):
+        """Test the load virtual_memory"""
+        self.assertEqual(self.ram.load.virtual_memory, 85.7)
 
-    def test_003_data_parsed(self):
-        """Test if not empty""" #TODO : change doc
-        self.assertNotEqual(self.ram.data, {})
+    def test_load_dict(self):
+        """Test the load dict structure"""
+        self.assertEqual(self.ram.load.to_dict()['memory'], self.ram.load.memory)
 
-    def test_004_loads_structure(self):
-        """Test the loads structure"""
-        #print(type(self.cpu.clocks))
-        self.assertIn('memory', self.ram.loads)
-        self.assertIn('virtual_memory', self.ram.loads)
-
-    def test_005_data_structure(self):
-        """Test the data structure"""
-        #print(type(self.cpu.clocks))
-        self.assertIn('memory_used', self.ram.data)
-        self.assertIn('memory_available', self.ram.data)
-        self.assertIn('virtual_memory_used', self.ram.data)
-        self.assertIn('virtual_memory_available', self.ram.data)
+    def test_data_memory_used(self):
+        """Test the data memory_used"""
+        self.assertEqual(self.ram.data.memory_used, 6.5)
+    
+    def test_data_memory_available(self):
+        """Test the data memory_available"""
+        self.assertEqual(self.ram.data.memory_available, 1.3)
+    
+    def test_data_virtual_memory_used(self):
+        """Test the data virtual_memory_used"""
+        self.assertEqual(self.ram.data.virtual_memory_used, 13.2)
+    
+    def test_data_virtual_memory_available(self):
+        """Test the data virtual_memory_available"""
+        self.assertEqual(self.ram.data.virtual_memory_available, 2.2)
+    
+    def test_data_dict(self):
+        """Test the data dict structure"""
+        self.assertEqual(self.ram.data.to_dict()['memory_used'], self.ram.data.memory_used)
